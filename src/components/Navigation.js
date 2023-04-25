b77f7d4e32ca9c65bbb8b5688ab5dbe735f1394d
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-04-25 21:01:29 +0600

    TechoWebsite

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <div className="links">
+        <Link></Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
---
+++ src/screens/AdaptivePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdaptivePage = () => {
+  return <div>AdaptivePage</div>;
+};
+
+export default AdaptivePage;
