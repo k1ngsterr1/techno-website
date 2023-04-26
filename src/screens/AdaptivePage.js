d7fd6cb5b486ba2b938c7f2164d36f5ea50afed2
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-04-26 18:00:10 +0600

    TechoWebsite V3

--- src/screens/AdaptivePage.js
+++ src/screens/AdaptivePage.js
@@ -2,12 +2,33 @@ import React from "react";
 
 import styles from "../styles/adaptive-styles/adaptive.css";
 
+import { AiFillCopy } from "react-icons/ai";
+
 const AdaptivePage = () => {
+  function VwContainer(props) {
+    return (
+      <div className="vw-container">
+        <h3 className="vw-heading">{props.heading}</h3>
+        <p className="vw-paragraph">{props.paragraph}</p>
+        <input className="vw-input"></input>
+        <div className="result-container">
+          <span className="result">{props.result}</span>
+          <AiFillCopy></AiFillCopy>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="adaptive-page">
       <div className="container">
         <h2 className="adaptive-heading">Adaptive</h2>
-        <div className="main-content"></div>
+        <div className="main-content adaptive-container">
+          <VwContainer
+            heading="Pixels in VW (DESKTOP)"
+            paragraph="Convert From px to VW"
+          ></VwContainer>
+        </div>
       </div>
     </div>
   );
